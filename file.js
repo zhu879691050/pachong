b23c07bd44529829d87528ef609930dd52099f63
爱吃葫芦娃 <879691050@qq.com>
2017-01-23 01:02:11 +0800

    处理文件的
    可以将爬取的各个页面合并到一个文件中

---
+++ file.js
@@ -0,0 +1,49 @@
+"use strict"
+const request = require('request')
+const cheerio = require('cheerio')
+const fs = require('fs')
+var log = function() {
+    console.log.apply(console, arguments)
+}
+const moviesAll = []
+const handle = function(path){
+  fs.readFile(path, function(err, data){
+    if (err == null) {
+      const s = JSON.parse(data)
+      // const s = data
+      log('---', s.length)
+      for (var i = 0; i < s.length; i++) {
+          moviesAll.push(s[i])
+      }
+      log("---moviesAll", moviesAll.length)
+      saveMovies(moviesAll)
+    } else {
+      log(err)
+    }
+
+  })
+}
+
+const saveMovies = function(movies) {
+    const fs = require('fs')
+    const path = '动画.txt'
+    const data = JSON.stringify(movies, null, 2)
+    fs.writeFile(path, data, function(error){
+        if (error !== null) {
+          log('****写入文档失败', error)
+        } else {
+          log('----写入成功')
+        }
+    })
+}
+
+const __main = function(){
+    const j = 5
+    for (var i = 0; i < j; i++) {
+      const path = i + '.txt'
+      handle(path)
+      log('00000', moviesAll)
+    }
+}
+
+__main()
